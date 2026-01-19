4ce5f785d32207e58966a493129025d735876d87
Soham Kotnala <sohamkotnalano.1@gmail.com>
2026-01-20 00:48:43 +0530

    Ensure API base URL uses /api

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "../utils/auth";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL, // MUST include /api
 });
 
 /* 🔐 AUTO ATTACH JWT */
